4b4358afd338cd8a998abd4f6dd8614a4935be1e
dustin-biser <dustinbiser@gmail.com>
2016-03-09 14:09:19 -0500

    Renamed Xcode project MetalSwift->MetalDemos

---
+++ Demos/OSX/Mipmap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mipmap
+//
+//  Created by Dustin on 3/7/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Demos/OSX/Mipmap/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  Mipmap
+//
+//  Created by Dustin on 3/7/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
